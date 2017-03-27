beb89cf1b0226a0081a383170f69b50af39c118f
Alexander Suhodolov <alexander.suhodolov@hiqo-solutions.com>
2017-03-27 16:46:31 +0300

    Add errorDescription to ThaliCoreError

--- ThaliCore/Core/ThaliCoreError.swift
+++ ThaliCore/Core/ThaliCoreError.swift
@@ -7,7 +7,7 @@
 //  See LICENSE.txt file in the project root for full license information.
 //
 
-public enum ThaliCoreError: String, Error {
+public enum ThaliCoreError: String, CustomNSError, LocalizedError {
 
   case StartListeningNotActive = "startListeningForAdvertisements is not active"
   case ConnectionFailed = "Connection could not be established"
@@ -18,4 +18,44 @@ public enum ThaliCoreError: String, Error {
   case RadioTurnedOff = "Radio Turned Off"
   case UnspecifiedRadioError = "Unspecified Error with Radio infrastructure"
   case IllegalPeerID = "Illegal peerID"
+  
+  public static var errorDomain: String {
+    return "ThaliCoreError"
+  }
+  
+  public var errorCode: Int {
+    switch self {
+    case .StartListeningNotActive:
+      return 0
+    case .ConnectionFailed:
+      return 1
+    case .ConnectionTimedOut:
+      return 2
+    case .MaxConnectionsReached:
+      return 3
+    case .NoNativeNonTCPSupport:
+      return 4
+    case .NoAvailableTCPPorts:
+      return 5
+    case .RadioTurnedOff:
+      return 6
+    case .UnspecifiedRadioError:
+      return 7
+    case .IllegalPeerID:
+      return 8
+    }
+  }
+  
+  /// The user-info dictionary.
+  public var errorUserInfo: [String : Any] {
+    return ["ErrorDescription": description]
+  }
+  
+  public var description: String {
+    return rawValue
+  }
+  
+  public var errorDescription: String? {
+    return description
+  }
 }
